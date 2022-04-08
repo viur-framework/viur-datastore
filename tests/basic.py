67705b63dee039fe38cfc008fe19f210a688f76e
Tobias Steinrücken <ts@mausbrand.de>
2022-04-08 11:18:47 +0200

    Replaced "test-kind" constant with configurable testKindName in basic function test class

--- tests/basic.py
+++ tests/basic.py
@@ -1,6 +1,6 @@
 import unittest
 from viur import datastore
-from .base import BaseTestClass, datastoreSampleValues, viurTypeToGoogleType
+from .base import BaseTestClass, datastoreSampleValues, viurTypeToGoogleType, testKindName
 
 """
 	This test-set ensures that basic operations as get, put and delete work as expected
@@ -12,60 +12,60 @@ class BasicFunctionTest(BaseTestClass):
 		"""
 			Ensure we can store a simple entity
 		"""
-		self.assertTrue(self.datastoreClient.get(self.datastoreClient.key("test-kind", "test-entity")) is None)
-		entity = datastore.Entity(datastore.Key("test-kind", "test-entity"))
+		self.assertTrue(self.datastoreClient.get(self.datastoreClient.key(testKindName, "test-entity")) is None)
+		entity = datastore.Entity(datastore.Key(testKindName, "test-entity"))
 		datastore.Put(entity)
-		self.assertTrue(self.datastoreClient.get(self.datastoreClient.key("test-kind", "test-entity")) is not None)
+		self.assertTrue(self.datastoreClient.get(self.datastoreClient.key(testKindName, "test-entity")) is not None)
 
 	def test_get(self):
 		"""
 			Ensure we can retrieve a simple entity
 		"""
-		self.assertTrue(self.datastoreClient.get(self.datastoreClient.key("test-kind", "test-entity")) is None)
-		self.assertTrue(datastore.Get(datastore.Key("test-kind", "test-entity")) is None)
-		e = self.datastoreClient.entity(self.datastoreClient.key("test-kind", "test-entity"))
+		self.assertTrue(self.datastoreClient.get(self.datastoreClient.key(testKindName, "test-entity")) is None)
+		self.assertTrue(datastore.Get(datastore.Key(testKindName, "test-entity")) is None)
+		e = self.datastoreClient.entity(self.datastoreClient.key(testKindName, "test-entity"))
 		self.datastoreClient.put(e)  # Create the entity
-		self.assertTrue(self.datastoreClient.get(self.datastoreClient.key("test-kind", "test-entity")) is not None)
-		self.assertTrue(datastore.Get(datastore.Key("test-kind", "test-entity")) is not None)
+		self.assertTrue(self.datastoreClient.get(self.datastoreClient.key(testKindName, "test-entity")) is not None)
+		self.assertTrue(datastore.Get(datastore.Key(testKindName, "test-entity")) is not None)
 
 	def test_delete(self):
 		"""
 			Ensure we can delete a entity
 		"""
-		e = self.datastoreClient.entity(self.datastoreClient.key("test-kind", "test-entity"))
+		e = self.datastoreClient.entity(self.datastoreClient.key(testKindName, "test-entity"))
 		self.datastoreClient.put(e)  # Create the entity
-		self.assertTrue(self.datastoreClient.get(self.datastoreClient.key("test-kind", "test-entity")) is not None)
-		datastore.Delete(datastore.Key("test-kind", "test-entity"))
-		self.assertTrue(self.datastoreClient.get(self.datastoreClient.key("test-kind", "test-entity")) is None)
+		self.assertTrue(self.datastoreClient.get(self.datastoreClient.key(testKindName, "test-entity")) is not None)
+		datastore.Delete(datastore.Key(testKindName, "test-entity"))
+		self.assertTrue(self.datastoreClient.get(self.datastoreClient.key(testKindName, "test-entity")) is None)
 
 	def test_delete_non_existant(self):
 		"""
 			Deleting a non-existing entity should not fail
 		"""
-		self.assertTrue(self.datastoreClient.get(self.datastoreClient.key("test-kind", "test-entity")) is None)
-		datastore.Delete(datastore.Key("test-kind", "test-entity"))
-		self.assertTrue(self.datastoreClient.get(self.datastoreClient.key("test-kind", "test-entity")) is None)
+		self.assertTrue(self.datastoreClient.get(self.datastoreClient.key(testKindName, "test-entity")) is None)
+		datastore.Delete(datastore.Key(testKindName, "test-entity"))
+		self.assertTrue(self.datastoreClient.get(self.datastoreClient.key(testKindName, "test-entity")) is None)
 
 	def test_delete_empty_list(self):
 		"""
 			Deleting a emty list of keys should just be ignored
 		"""
-		self.assertTrue(self.datastoreClient.get(self.datastoreClient.key("test-kind", "test-entity")) is None)
+		self.assertTrue(self.datastoreClient.get(self.datastoreClient.key(testKindName, "test-entity")) is None)
 		datastore.Delete([])
-		self.assertTrue(self.datastoreClient.get(self.datastoreClient.key("test-kind", "test-entity")) is None)
+		self.assertTrue(self.datastoreClient.get(self.datastoreClient.key(testKindName, "test-entity")) is None)
 
 	def test_datatypes(self):
 		"""
 			Ensure that we can store and retrieve all supported python types
 		"""
-		entity = datastore.Entity(datastore.Key("test-kind", "test-entity"))
+		entity = datastore.Entity(datastore.Key(testKindName, "test-entity"))
 		entity.update(datastoreSampleValues)
 		datastore.Put(entity)
-		entity2 = datastore.Get(datastore.Key("test-kind", "test-entity"))
+		entity2 = datastore.Get(datastore.Key(testKindName, "test-entity"))
 		for k, v in datastoreSampleValues.items():
 			self.assertEqual(entity2[k], v)
 		# Also, double-check with the original google API
-		entity3 = self.datastoreClient.get(self.datastoreClient.key("test-kind", "test-entity"))
+		entity3 = self.datastoreClient.get(self.datastoreClient.key(testKindName, "test-entity"))
 		for k, v in datastoreSampleValues.items():
 			self.assertEqual(entity3[k], viurTypeToGoogleType(v))
 
@@ -75,10 +75,10 @@ class BasicFunctionTest(BaseTestClass):
 			Ensure, we can store an empty list.
 		"""
 		testList = []
-		entity = datastore.Entity(datastore.Key("test-kind", "test-entity"))
+		entity = datastore.Entity(datastore.Key(testKindName, "test-entity"))
 		entity["testlist"] = testList
 		datastore.Put(entity)
-		entity = datastore.Get(datastore.Key("test-kind", "test-entity"))
+		entity = datastore.Get(datastore.Key(testKindName, "test-entity"))
 		self.assertEqual(entity["testlist"], testList)
 
 	def test_unindexed_list(self):
@@ -87,11 +87,11 @@ class BasicFunctionTest(BaseTestClass):
 			Ensure, we can set a list to un-indexed.
 		"""
 		testList = ["a"*600, "b"*600]
-		entity = datastore.Entity(datastore.Key("test-kind", "test-entity"))
+		entity = datastore.Entity(datastore.Key(testKindName, "test-entity"))
 		entity["testlist"] = testList
 		entity.exclude_from_indexes.add("testlist")
 		datastore.Put(entity)
-		entity = datastore.Get(datastore.Key("test-kind", "test-entity"))
+		entity = datastore.Get(datastore.Key(testKindName, "test-entity"))
 		self.assertEqual(entity["testlist"], testList)
 		self.assertTrue("testlist" in entity.exclude_from_indexes)
 
@@ -101,10 +101,10 @@ class BasicFunctionTest(BaseTestClass):
 			Ensure, we can store an indexed list.
 		"""
 		testList = ["a"*300, "b"*300]
-		entity = datastore.Entity(datastore.Key("test-kind", "test-entity"))
+		entity = datastore.Entity(datastore.Key(testKindName, "test-entity"))
 		entity["testlist"] = testList
 		datastore.Put(entity)
-		entity = datastore.Get(datastore.Key("test-kind", "test-entity"))
+		entity = datastore.Get(datastore.Key(testKindName, "test-entity"))
 		self.assertEqual(entity["testlist"], testList)
 		self.assertFalse("testlist" in entity.exclude_from_indexes)
 
