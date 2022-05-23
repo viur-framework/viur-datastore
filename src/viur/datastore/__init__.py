f26a20347b840f339dfd87d44297c1762b9ec0ef
Stefan Kögl <sk@mausbrand.de>
2022-05-23 23:52:53 +0200

    filling _all_ export/import with important new exception hierarchy

--- src/viur/datastore/__init__.py
+++ src/viur/datastore/__init__.py
@@ -1,13 +1,49 @@
 from viur.datastore.types import KEY_SPECIAL_PROPERTY, DATASTORE_BASE_TYPES, SortOrder, SkelListRef, Entity, \
 	QueryDefinition, Key, currentDbAccessLog
 from viur.datastore.query import Query
-from viur.datastore.utils import fixUnindexableProperties, normalizeKey, keyHelper, IsInTransaction, GetOrInsert,\
+from viur.datastore.utils import fixUnindexableProperties, normalizeKey, keyHelper, IsInTransaction, GetOrInsert, \
 	encodeKey, acquireTransactionSuccessMarker, startDataAccessLog, endDataAccessLog
-from viur.datastore.transport import Get, Put, Delete, RunInTransaction, Collision, AllocateIDs
+from viur.datastore.transport import Get, Put, Delete, RunInTransaction, AllocateIDs
 from viur.datastore.config import conf as config
+from viur.datastore.errors import *
 
-__all__ = ["KEY_SPECIAL_PROPERTY", "DATASTORE_BASE_TYPES", "SortOrder", "SkelListRef", "Entity", "QueryDefinition",
-		   "Key", "Query", "fixUnindexableProperties", "normalizeKey", "keyHelper", "Get", "Put", "Delete",
-		   "RunInTransaction", "IsInTransaction", "currentDbAccessLog", "GetOrInsert", "encodeKey",
-		   "acquireTransactionSuccessMarker", "Collision", "AllocateIDs", "config", "startDataAccessLog",
-		   "endDataAccessLog"]
+__all__ = [
+	"KEY_SPECIAL_PROPERTY",
+	"DATASTORE_BASE_TYPES",
+	"SortOrder",
+	"SkelListRef",
+	"Entity",
+	"QueryDefinition",
+	"Key",
+	"Query",
+	"fixUnindexableProperties",
+	"normalizeKey",
+	"keyHelper",
+	"Get",
+	"Put",
+	"Delete",
+	"RunInTransaction",
+	"IsInTransaction",
+	"currentDbAccessLog",
+	"GetOrInsert",
+	"encodeKey",
+	"acquireTransactionSuccessMarker",
+	"AllocateIDs",
+	"config",
+	"startDataAccessLog",
+	"endDataAccessLog",
+	"ViurDatastoreError",
+	"AbortedError",
+	"AlreadyExistsError",
+	"DeadlineExceededError",
+	"FailedPreconditionError",
+	"InternalError",
+	"InvalidArgumentError",
+	"NotFoundError",
+	"PermissionDeniedError",
+	"ResourceExhaustedError",
+	"UnauthenticatedError",
+	"UnavailableError",
+	"NoMutationResultsReceived",
+	"WrongNumberMutationResultsReceived",
+]
